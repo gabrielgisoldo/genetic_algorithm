c76a73cd9992eb90029ac1e463784cc9e0f31ef9
gabrielgisoldo <gabrielgisoldi@hotmail.com>
2017-06-21 20:00:22 -0300

    Adicionado teste com lista.

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import unittest
 from acharString import AcharString
+from acharLista import AcharLista
 
 
 class Tests(unittest.TestCase):
     """."""
 
-    def test(self):
+    def test_string(self):
         """."""
+        print "Teste com String\n"
+
         tamanho_obj = 10
         tamanho_pop = 500
         t = AcharString(
@@ -21,5 +24,21 @@ class Tests(unittest.TestCase):
         for key in m.keys():
             print '%s: %s\n' % (key, m[key])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_lista(self):
+        """."""
+        print "Teste com Lista\n"
+
+        t = AcharLista(
+            minimo=0, maximo=100, tamanho_genes=18, objetivo=1750)
+
+        melhor, obj = t.iniciar()
+
+        m = vars(melhor)
+
+        print "objetivo: %s\n" % (obj)
+
+        for key in m.keys():
+            print '%s: %s\n' % (key, m[key])
+
+suite = unittest.TestLoader().loadTestsFromTestCase(Tests)
+unittest.TextTestRunner(verbosity=2).run(suite)
